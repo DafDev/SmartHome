55a0b28ce5cf0057670df317f53e2383a60c94d5
Daf <afahd.ali@gmail.com>
2017-12-04 17:29:27 +0100

    Movement sensr module - Room entry function coded but not tested yet

--- movementAndLights.py
+++ movementAndLights.py
@@ -1,17 +1,35 @@
 import GPIO.out as GPIO
+import time
 
 led = 4
 movementSensorAtDoor = 17
 movementSensorInsideRoom = 27
+switchLights = 22
 personsInsideRoom = 0
+isLightUp = False
+stepInOutTime = 0.5
 
-GPIO.setup(led,GPIO.INPUT)
-GPIO.setup(movementSensorAtDoor, GPIO.OUTPUT)
-GPIO.setup(movementSensorInsideRoom GPIO.OUTPUT)
+GPIO.setup(led,GPIO.OUT)
+GPIO.setup(movementSensorAtDoor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(movementSensorInsideRoom, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(switchLights, GPIO.IN, pull_up_up=GPIO.PUD_UP)
 
-def entryPersons(){
-    if (movementSensorAtDoor) then:
-        if (movementSensorInsideRoom):
-            led.High
-            personsInsideRoom++
-}
+
+def entryPersons():
+    global personsInsideRoom
+    global isLightUp
+    if GPIO.input(movementSensorAtDoor):
+        time.sleep(stepInOutTime)
+        if GPIO.input(movementSensorInsideRoom):
+            isLightUp = True
+            personsInsideRoom = personsInsideRoom + 1
+            GPIO.output(led, isLightUp)
+            print("Number of people inside: " + str(personsInsideRoom))
+            return personsInsideRoom, isLightUp
+
+GPIO.output(led, False)
+try:
+    while true:
+        entryPersons()
+except KeyboardInterrupt():
+    GPIO.cleanup()
